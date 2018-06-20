42897feecf93dbff5f840303960a1803651f5c3b
Kyle E. Mitchell <kyle@kemitchell.com>
2018-06-19 23:11:54 -0700

    list-*: List licenses containing strings

--- list.js
+++ list.js
@@ -2,26 +2,33 @@ var fs = require('fs')
 var path = require('path')
 
 module.exports = function (type) {
-  var strings = []
+  var strings = {}
 
   fs.readdirSync('licenses').forEach(function (file) {
     var data = require('./licenses/' + file)
-    recurse(data)
+    var basename = path.basename(file, '.json')
+    recurse(basename, data)
   })
 
-  strings.sort().forEach(function (string) {
-    console.log(string)
-  })
+  Object.keys(strings)
+    .sort()
+    .forEach(function (string) {
+      console.log(string + ' (' + strings[string].sort().join(', ') + ')')
+    })
 
-  function recurse (data) {
+  function recurse (basename, data) {
     if (Array.isArray(data[type])) {
       data[type].forEach(function (string) {
-        if (!strings.includes(string)) strings.push(string)
+        if (!strings.hasOwnProperty(string)) {
+          strings[string] = [basename]
+        } else if (!strings[string].includes(basename)) {
+          strings[string].push(basename)
+        }
       })
     }
     if (Array.isArray(data.content)) {
       data.content.forEach(function (element) {
-        recurse(element)
+        recurse(basename, element)
       })
     }
   }
